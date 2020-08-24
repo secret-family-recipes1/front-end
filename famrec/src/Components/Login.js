f156e0e42c22a45697bab79d1bbb5689f713021b
kevin lam <kevin@kevins-MacBook-Pro-3.local>
2020-08-24 13:46:27 -0700

    login and signup forms working with app and api

--- famrec/src/Components/Login.js
+++ famrec/src/Components/Login.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
+import FormSchema from './validation/FormSchema'
+
 
 class Login extends Component {
 	constructor(props) {
@@ -12,7 +15,7 @@ class Login extends Component {
 
 		this.update = this.update.bind(this);
 
-		this.displayLogin = this.displayLogin.bind(this);
+		// this.displayLogin = this.displayLogin.bind(this);
 	}
 
 	update(e) {
@@ -23,20 +26,46 @@ class Login extends Component {
 		});
 	}
 
-	displayLogin(e) {
-		e.preventDefault();
-		console.log('You are logged in');
-		console.log(this.state);
-		this.setState({
-			email: '',
-			password: ''
-		});
-	}
+	// displayLogin(e) {
+	// 	e.preventDefault();
+	// 	console.log('You are logged in');
+	// 	console.log(this.state);
+	// 	this.setState({
+	// 		email: '',
+	// 		password: ''
+	// 	});
+    // }
+    
+    onSubmit = e => {
+		e.preventDefault()
+		if (
+		  !this.state.email.trim() ||
+		  !this.state.password.trim())
+		{
+		  return 
+		} else {
+			console.log('registering')
+			console.log(this.props.history)
+			axios
+			.post('https://back-end-recipes.herokuapp.com/api/auth/login', this.state)
+	
+			.then(res => {
+				console.log('response', res.data.token)
+				localStorage.setItem('jwt', res.data.token);
+				this.props.history.push('/recipes');                    
+			})
+			.catch(err => {
+				console.log(err);
+				console.error(err)
+				alert("Login failed. Please check username and password.");
+			})
+		}
+	} 
 
 	render() {
 		return (
 			<div className="login">
-				<form onSubmit={this.displayLogin}>
+				<form onSubmit={this.onSubmit}>
 					<h2>Login</h2>
 					<div className="username">
 						<input
--- famrec/src/Components/SignUp.js
+++ famrec/src/Components/SignUp.js
@@ -1,52 +1,113 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import axios from 'axios'
+import FormSchema from './validation/FormSchema'
+import * as yup from 'yup'
 
 class SignUp extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			fullname: '',
+			firstname: '',
+			lastname: '',
 			email: '',
 			password: ''
 		};
 
 		this.update = this.update.bind(this);
-
-		this.displayLogin = this.displayLogin.bind(this);
+		// this.displayLogin = this.displayLogin.bind(this);
 	}
 
-	update(e) {
-		let name = e.target.name;
-		let value = e.target.value;
+	update(name, value) {
+		yup
+		.reach(FormSchema, name)
+		.validate(value)
+		.then(valid => {
+		  setFormErrors({
+			...formErrors,
+			[name]: "",
+		  })
+		})
+		.catch(err => {
+		  setFormErrors({
+			...formErrors,
+			[name]: err.errors[0],
+		  })
+		})
+		// let name = e.target.name;
+		// let value = e.target.value;
 		this.setState({
 			[name]: value
 		});
 	}
 
-	displayLogin(e) {
-		e.preventDefault();
-		console.log('You have successfully registered');
-		console.log(this.state);
-		this.setState({
-			fullname: '',
-			email: '',
-			password: ''
-		});
-	}
+	// displayLogin(e) {
+	// 	e.preventDefault();
+	// 	console.log('You have successfully registered');
+	// 	console.log(this.state);
+	// 	this.setState({
+	// 		firstName: '',
+	// 		lastName: '',
+	// 		email: '',
+	// 		password: ''
+	// 	});
+	// }
+
+
+	onSubmit = e => {
+		e.preventDefault()
+		if (
+		  !this.state.firstname.trim() ||
+		  !this.state.lastname.trim() ||
+		  !this.state.email.trim() ||
+		  !this.state.password.trim())
+		{
+		  return 
+		} else {
+			console.log('registering')
+			console.log(this.props.history)
+			axios
+			.post('https://back-end-recipes.herokuapp.com/api/auth/register', this.state)
+		   
+			.then(res => {
+				console.log('response', res.data.token)
+				localStorage.setItem('jwt', res.data.token);
+				// localStorage.setItem('isLoggedIn', true);
+				// localStorage.setItem('user_id', res.data.id);
+				// window.location.reload();
+				this.props.history.push('/login');                    
+			})
+			.catch(err => {
+				console.log(err);
+				console.error(err)
+				alert("Login failed. Please check username and password.");
+			})
+		}
+	} 
+
 
 	render() {
 		return (
 			<div className="register">
-				<form onSubmit={this.displayLogin}>
+				<form onSubmit={this.onSubmit}>
 					<h2>Register</h2>
 
 					<div className="name">
 						<input
 							type="text"
-							placeholder="Full Name"
-							name="fullname"
-							value={this.state.fullname}
+							placeholder="First Name"
+							name="firstname"
+							value={this.state.firstname}
+							onChange={this.update}
+						/>
+					</div>
+					<div className="name">
+						<input
+							type="text"
+							placeholder="Last Name"
+							name="lastname"
+							value={this.state.lastname}
 							onChange={this.update}
 						/>
 					</div>
@@ -75,7 +136,7 @@ class SignUp extends Component {
 						<input type="password" placeholder="Confirm Password" name="password1" />
 					</div>
 
-					<input type="submit" value="Login" />
+					<input type="submit" value="Register" />
 				</form>
 
 				<Link to="/login ">Login Here</Link>
