e488df81a96c712a6180819a303b43ebb71be196
kevin lam <kevin@kevins-MacBook-Pro-3.local>
2020-08-26 12:02:22 -0700

    some styling for the search bar

--- famrec/src/Components/HomePage.js
+++ famrec/src/Components/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './HomePage.css';
+import '../styles/HomePage.css';
 import RecipeListView from './RecipeListView';
 import Styled from 'styled-components'
 
@@ -8,11 +8,15 @@ width: 90%;
 max-width: 1200px;
 margin: 2rem auto;
 text-align: center;
+h2 {
+    text-shadow: 2px 2px 4px white, -1px -1px 4px white;
+    font-size: 3rem;
+    text-transform: capitalize;
+}
 `
 
 // Home page
 const HomePage = () => {
-
         return (
             <StyledDiv>
                 <h2>Your family's secret recipes</h2>
--- famrec/src/Components/RecipeListView.js
+++ famrec/src/Components/RecipeListView.js
@@ -6,6 +6,15 @@ import Styled from 'styled-components'
 
 import RecipeCard from './RecipeList'
 
+const StyledSearch = Styled.input`
+text-align: center;
+border-radius: 20px;
+padding: 0.25rem;
+width: 33%;
+margin: 2rem;
+box-shadow: 0 0 10px black;
+`
+
 const StyledDiv = Styled.div`
 display: flex;
 justify-content: center;
@@ -35,27 +44,26 @@ const RecipeListView = (props) => {
 
 return (
     <div>
-        <input
+        <StyledSearch
         placeholder='Search'
         name='search'
         onChange={handleSearch}
         value={search}
-        ></input>
-    <StyledDiv>
-        {(props.recipes != undefined && props.recipes.length > 0) ? (<div>{
-        props.recipes
-        .filter(recipe => {
-            return recipe.name.includes(search)
-        })
-        .map(recipe => {
-            return <RecipeCard recipe={recipe} key={recipe.id}/>
-        })
-        } </div>) : <p>No recipes</p> }
-    </StyledDiv>
+        ></StyledSearch>
+        <StyledDiv>
+            {(props.recipes != undefined && props.recipes.length > 0) ? (<div>{
+            props.recipes
+            .filter(recipe => {
+                return (recipe.name.includes(search) || recipe.category.includes(search))
+            })
+            .map(recipe => {
+                return <RecipeCard recipe={recipe} key={recipe.id}/>
+            })
+            } </div>) : <p>No recipes</p> }
+        </StyledDiv>
     </div>
 )
 }
-
 
 const mapStateToProps = state => {
     return {
